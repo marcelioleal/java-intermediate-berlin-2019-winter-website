d1df4f2b66e723e546277bdfe53ea6a0032b1690
alinadenisenko <a.sharapova.a@gmail.com>
2019-09-05 01:04:33 +0200

    lesson5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,16 +4,17 @@ module.exports = {
   themeConfig: {
     title: 'Redi School Intermediate Java',
     sidebar: {'/lessons': [
-      '/lessons/01-intro',
-      '/lessons/02-git',
-      '/lessons/03-string-operations'
-    ],
-    '/teachers': [
-      '/teachers/for-teachers',
-      '/teachers/how-to-edit-this-site',
-      '/teachers/how-to-make-a-screencast',
-      '/teachers/how-to-teach-resources'
-    ]},
+        '/lessons/01-intro',
+        '/lessons/02-git',
+        '/lessons/03-string-operations',
+        '/lessons/05-oop-objects-classes'
+      ],
+      '/teachers': [
+        '/teachers/for-teachers',
+        '/teachers/how-to-edit-this-site',
+        '/teachers/how-to-make-a-screencast',
+        '/teachers/how-to-teach-resources'
+      ]},
     sidebarDepth: 1,
     displayAllHeaders: 0,
     nav: [
